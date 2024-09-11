7d648b82a0329c4571f474a7ef861e806e712457
Ivan <ovchinno.ivan@gmail.com>
2024-09-11 13:34:17 +0300

    fixed naming constants, vars and other fixed

--- main.cpp
+++ main.cpp
@@ -1,100 +1,108 @@
 #include <iostream>
 
-int main(int, char**) {
-    const int kMaxHours = 23;
-    const int kMinHours = 0;
-    const int kMaxMinutes = 60;
-    const int kMinMinutes = 0;
-    const int kMorningStart = 5;
-    const int kMorningEnd = 11;
-    const int kAfternoonStart = 12;
-    const int kAfternoonEnd = 16;
-    const int kEveningStart = 17;
-    const int kEveningEnd = 21;
-    const int kMidnightHour = 0;
-    const int kNoonHour = 12;
-    const int kSingleHour = 1;
-    const int kFewHoursEnd = 4;
-    const int kTeenStart = 11;
-    const int kTeenEnd = 14;
-    const int kSingleMinuteRemainder = 1;
-    const int kFewMinutesEnd = 4;
-    const int kDecimel = 10;
+namespace {
+const int kMaxHours = 23;
+const int kMinHours = 0;
 
-    int h = 0;
-    int m = 0;
+const int kMaxMinutes = 59;
+const int kMinMinutes = 0;
+
+const int kMorningStart = 5;
+const int kMorningEnd = 11;
+
+const int kAfternoonStart = 12;
+const int kAfternoonEnd = 16;
+
+const int kEveningStart = 17;
+const int kEveningEnd = 21;
+
+const int kMidnightHour = 0;
+const int kNoonHour = 12;
+
+const int kHoursFormat = 12;
+
+const int kHoursGenetiveCaseMin = 0;
+const int kHoursGenetiveCaseMax = 4;
+const int kHoursNominativeCase = 1;
+
+const int kExactMinutes = 0;
+
+const int kMinutesNominativeCase = 1;
+const int kMinutesNominativeCasePeriodMin = 5;
+const int kMinutesNominativeCasePeriodMax = 9;
+const int kMinutesNominativeCaseMin = 10;
+const int kMinutesNominativeCaseMax = 20;
+const int kMinutesGenetiveCaseMin = 2;
+const int kMinutesGenetiveCaseMax = 4;
+
+const int kDecimal = 10;
+}  // namespace
+
+int main(int, char**) {
+    int hours = 0;
+    int minutes = 0;
 
     std::cout << "Введите часы и минуты через пробел\n";
-    std::cin >> h;
-    std::cin >> m;
+    std::cin >> hours >> minutes;
 
-    if (h > kMaxHours || h < kMinHours) {
-        std::cout << "Wrong hours input\n";
+    if (hours > kMaxHours || hours < kMinHours) {
+        std::cout << "Неверный ввод значения часа, возможные значениея от 0 до 23 включительно\n";
         return 1;
     }
 
-    if (m > kMaxMinutes || m < kMinMinutes) {
-        std::cout << "Wrong minutes input\n";
+    if (minutes > kMaxMinutes || minutes < kMinMinutes) {
+        std::cout << "Неверный ввод значения минут, возможные значения от 0 до 59 включительно\n";
         return 1;
     }
 
-    if (h == kMidnightHour && m == kMidnightHour) {
+    if (hours == kMidnightHour && minutes == kExactMinutes) {
         std::cout << "Полночь\n";
         return 0;
     }
 
-    if (h == kNoonHour && m == kMidnightHour) {
+    if (hours == kNoonHour && minutes == kExactMinutes) {
         std::cout << "Полдень\n";
         return 0;
     }
 
-    int timeOfDay = 0;
+    int formatedHours = hours;
+    if (formatedHours > kHoursFormat) {
+        formatedHours -= kHoursFormat;
+    }
 
-    if (h >= kMorningStart && h <= kMorningEnd) {
-        timeOfDay = 1;
-    } else if (h >= kAfternoonStart && h <= kAfternoonEnd) {
-        timeOfDay = 2;
-    } else if (h >= kEveningStart && h <= kEveningEnd) {
-        timeOfDay = 3;
+    if (hours == kHoursNominativeCase) {
+        std::cout << formatedHours << " час ";
+    } else if (hours >= kHoursGenetiveCaseMin && hours <= kHoursGenetiveCaseMax) {
+        std::cout << formatedHours << " часа ";
     } else {
-        timeOfDay = 4;
+        std::cout << formatedHours << " часов ";
     }
 
-    if (h > kNoonHour) {
-        h -= kNoonHour;
+    if (minutes != kExactMinutes) {
+        if ((minutes % kDecimal >= kMinutesNominativeCasePeriodMin && minutes % kDecimal <= kMinutesNominativeCasePeriodMax) ||
+            (minutes >= kMinutesNominativeCaseMin && minutes <= kMinutesNominativeCaseMax)) {
+            std::cout << minutes << " минут ";
+        } else if (minutes % kDecimal >= kMinutesGenetiveCaseMin && minutes % kDecimal <= kMinutesGenetiveCaseMax) {
+            std::cout << minutes << " минуты ";
+        } else {
+            std::cout << minutes << " минута ";
+        }
     }
 
-    if (h == kSingleHour) {
-        std::cout << h << " час ";
-    } else if (h <= kFewHoursEnd && h != 0) {
-        std::cout << h << " часа ";
-    } else {
-        std::cout << h << " часов ";
-    }
-
-    if (m % kDecimel == kSingleMinuteRemainder && m != kMidnightHour) {
-        std::cout << m << " минута ";
-    } else if (m != kMidnightHour && m % kDecimel <= kFewMinutesEnd && m % kDecimel != kMidnightHour && !(m >= kTeenStart && m <= kTeenEnd)) {
-        std::cout << m << " минуты ";
-    } else if (m != kMidnightHour) {
-        std::cout << m << " минут ";
-    }
-
-    if (timeOfDay == 1) {
+    if (formatedHours >= kMorningStart && formatedHours <= kMorningEnd) {
         std::cout << "утра";
-    } else if (timeOfDay == 2) {
+    } else if (formatedHours >= kAfternoonStart && formatedHours <= kAfternoonEnd) {
         std::cout << "дня";
-    } else if (timeOfDay == 3) {
+    } else if (formatedHours >= kEveningStart && formatedHours <= kEveningEnd) {
         std::cout << "вечера";
     } else {
         std::cout << "ночи";
     }
 
-    if (m == kMidnightHour) {
-        std::cout << "ровно\n";
-    } else {
-        std::cout << "\n";
+    if (minutes == kExactMinutes) {
+        std::cout << " ровно";
     }
+    std::cout << "\n";
 
     return 0;
 }
