33e18c6aea6346a6fd4284a4b16923a98372ba28
Ivan <ovchinno.ivan@gmail.com>
2024-12-01 21:36:20 +0300

    Modify CMakeLists file, create vector realization

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
-#include "app.h"
+#include "collections/vector.h"
 
 int main(int argc, char** argv) {
-    test::helloWorld();
+    Vector::VecInt vector = Vector::CreateVector();
+    PrintVector(vector);
+
+    for (int i = 0; i < - 1; ++i) {
+        std::cout << i << std::endl;
+    }
+
+    PushBack(vector, 1);
+    PrintVector(vector);
+
+    const int a = 10;
+    PushBack(vector, a);
+    PrintVector(vector);
+
+    PushBack(vector, 3);
+    PrintVector(vector);
+
+    const int b = 20;
+    PushBack(vector, b);
+    PrintVector(vector);
+
+    GetElement(vector, 0) = 2;
+    PrintVector(vector);
+
+    Vector::DeleteVector(vector);
 
     for (int i = 0; i < argc; i++) {
         std::cout << argv[i] << std::endl;
